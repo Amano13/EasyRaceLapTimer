310a877a420be202f6ad6ca24c9988a68742f7fd
Amano13 <liam.goodrick@gmail.com>
2016-05-12 05:58:18 +1200

    Update 20151221120209_udp_broadcast_address_config_value.rb
    Change IP address.

--- web/db/migrate/20151221120209_udp_broadcast_address_config_value.rb
+++ web/db/migrate/20151221120209_udp_broadcast_address_config_value.rb
@@ -1,5 +1,5 @@
 class UdpBroadcastAddressConfigValue < ActiveRecord::Migration
   def change
-    ConfigValue::set_value("udp_broadcast_address","192.168.42.1")
+    ConfigValue::set_value("udp_broadcast_address","192.168.11.10")
   end
 end
